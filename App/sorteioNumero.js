b43004153a297853a27b043c6c195b6bbd9be059
israel <israelwendellc@gmail.com>
2023-07-31 19:38:28 -0300

    adicionando ./App/sorteioNumero.js

---
+++ App/sorteioNumero.js
@@ -0,0 +1,15 @@
+const numeroMenor = 1
+const numeroMaior = 100
+const numeroSecreto = geradoNumero ()
+
+
+function geradoNumero () {
+    return parseInt (Math.random () * numeroMaior +1)
+}
+
+const elementoMenor = document.querySelector ("#numero-menor")
+elementoMenor.innerText = numeroMenor
+const elementoMaior = document.querySelector ("#numero-maior")
+elementoMaior.innerText = numeroMaior
+
+console.log (numeroSecreto)
